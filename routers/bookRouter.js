2faf89d1fe19590dc1b98f17b7603c3e1fbfb541
jitendraprajapatt <72989611+jitendraprajapatt@users.noreply.github.com>
2025-05-26 23:33:17 +0530

    auth update

--- routers/bookRouter.js
+++ routers/bookRouter.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bookRouter = express.Router();
 const { getAllBooks, postBook, getBookById } = require('../controllers/book');
-
+const auth = require('../middleware/auth');
 // Routes
-bookRouter.post('/new/book', postBook);    
+bookRouter.post('/new/book',auth,postBook);    
 bookRouter.get('/books', getAllBooks);   
 bookRouter.get('/books/:id', getBookById); 
 
